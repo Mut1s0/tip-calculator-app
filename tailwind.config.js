3cf5c7c4eef5be8b2c3edf18d7f9732afb81948b
Allan Musyoka <musyokaallan96@gmail.com>
2024-04-16 11:47:24 +0300

    Added basic templates and elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary Colors
+        strongCyan: "hsl( 172, 67%, 45% )",
+
+        // Neutral Colors
+        veryDarkCyan: "hsl( 183, 100%, 15% )",
+        darkGrayishCyan: "hsl( 186, 14%, 84% )",
+        grayishCyan: "hsl( 184, 14%, 56% )",
+        lightGrayishCyan: "hsl( 185, 41%, 84% )",
+        veryLightGrayishCyan: "hsl( 189, 41%, 97% )",
+        white: "hsl( 0, 0%, 100% )"
+      },
+      fontFamily: {
+        spaceMono: [ "Space Mono", "monospace" ]
+      },
+      fontWeight: {
+        spaceMonoBold: "700"
+      },
+    },
+    screens: {
+      xs: "450px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    }
   },
   plugins: [],
 }
